a8cc38535dae5c876781ad2abf24a1cfc7b3259a
jahnavis3110 <128886645+jahnavis3110@users.noreply.github.com>
2023-07-25 19:52:47 +0530

    Create assignment.py

---
+++ assignment.py
@@ -0,0 +1,13 @@
+marks=int(input())
+if(marks >= 90):
+print("Grade:A+")
+elif(marks>=80 and marks<90):
+print("Grade:A")
+elif(marks>=70 and marks<80):
+print("Grade:B")
+elif(marks>=60 and marks<70):
+print("Grade:C")
+elif(marks>=50 and marks<60):
+print("Grade:D")
+else:
+print("Fail")
